dbcaafb773d47dc01a39e9f3bfe7127cd0b7aa6a
hank <hank.huiwang@gmail.com>
2019-03-25 12:13:41 +0800

    fix

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,4 +1,5 @@
 export default {
-	BASE_API_URL: "http://test.ywhub.com/api/"
+	//BASE_API_URL: "http://test.ywhub.com/api/",
+	BASE_API_URL: "https://api.jszioe.com/api/",
 	//BASE_API_URL: "http://cars.hank.com:8080/api/"
 }
